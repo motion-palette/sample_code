b16b543002610af223cd085afa4fa75bd73cf830
Suguru Ishizaki <suguru@cmu.edu>
2016-10-26 11:21:24 -0400

    Updating sample code - pyre ch12 and 13

---
+++ pyfe_ch13/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App
+
+def oauth():
+    return {"consumer_key": "",
+            "consumer_secret": "",
+            "token_key": "31144834-",
+            "token_secret": ""}
