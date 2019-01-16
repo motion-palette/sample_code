701167235fa97f376806a0560d7b59f2d4a3ed18
Suguru Ishizaki <suguru@cmu.edu>
2019-01-16 13:44:34 -0500

    latest

---
+++ pyfe_ch13/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App
+
+def oauth():
+    return {"consumer_key": "",
+            "consumer_secret": "",
+            "token_key": "",
+            "token_secret": ""}
